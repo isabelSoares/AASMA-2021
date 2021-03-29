97df9f5980caa5351bb3a6279adf02728c1fa228
João Dias <joao.dias12321@gmail.com>
2021-03-29 15:02:38 +0100

    agent - first

--- code/agent/agent.py
+++ code/agent/agent.py
@@ -1,15 +1,60 @@
 from ursina import *
+from time import sleep
+import random
 
-green_color = color.rgba(21,127,21)
-red_color = color.rgba(141,25,25)
+GREEN_COLOR = color.rgba(21,127,21)
+RED_COLOR = color.rgba(141,25,25)
+NORTH = 0
+SOUTH = 1
+WEST = 2
+EAST = 3
+DIRECTIONS = [Vec3(1,0,0), Vec3(0,0,-1), Vec3(-1,0,0), Vec3(0,0,1)]
 
 class Agent(Entity):
-    def __init__(self, model = 'cube', position = (0, 1, 0), color = color.rgba(0,0,0)):
+    def __init__(self, name = 'Unknown', model = 'cube', position = (0, 1, 0), color = color.rgba(0,0,0), direction = NORTH):
         super().__init__(
+            name = 'Agent ' + name,
             model = model,
             position = Vec3(position),
             color = color
         )
-
-agent_green = Agent(model = 'cube', position = (0,1,0), color = green_color)
-agent_red = Agent(model = 'cube', position = (2,1,0), color = red_color)
+        self.direction = direction
+    
+    def move(self):
+        if False: # TODO if there is a wall ahead
+            print(self.name + ': FAILED to move forward - WALL')
+            return
+        elif False: # TODO if there is a closed door ahead
+            print(self.name + ': FAILED to move forward - DOOR CLOSED')
+            return
+        print(self.name + ': moved forward')
+        self.position += DIRECTIONS[self.direction]
+    
+    def jump(self):
+        if False: # TODO if there is not a block ahead
+            print(self.name + ': FAILED to jumped forward - NO BLOCK')
+            return
+        elif False: # TODO if there is more than one block ahed
+            print(self.name + ': FAILED to jumped forward - VERY HIGH')
+            return
+        print(self.name + ': jumped forward')
+        self.position += DIRECTIONS[self.direction]
+        self.position += Vec3(0,1,0)
+    
+    def rotate_right(self):
+        print(self.name + ': rotated to the right')
+        self.rotation += Vec3(0, 90, 0)
+        self.direction += 1
+        if self.direction == 4:
+            self.direction = 0
+    
+    def rotate_left(self):
+        print(self.name + ': rotated to the left')
+        self.rotation -= Vec3(0, 90, 0)
+        self.direction -= 1
+        if self.direction == -1:
+            self.direction = 3
+    
+    def rotate_randomly(self):
+        random.choice([self.rotate_right, self.rotate_left])()
+        print('   - random choice')
--- code/maze.py
+++ code/maze.py
@@ -1,10 +1,11 @@
 from ursina import *
+from time import sleep
 app = Ursina()
 # Import environment
 from environment.setup import setup_window, setup_camera
 from environment.World import World
 # Import agent
-from agent import agent
+from agent.agent import *
 
 
 # Call construction of World
@@ -15,8 +16,21 @@ world = World(center, size)
 setup_window()
 setup_camera(position = Vec3(50,50,50), look_at = center)
 
+# Create agents
+AGENT_GREEN = Agent(name = 'GREEN', model = 'cube', position = (0,1,0), color = GREEN_COLOR)
+AGENT_RED = Agent(name = 'RED', model = 'cube', position = (2,1,0), color = RED_COLOR)
+
 def update():
+    sleep(0.1)
     if held_keys['q']:
         print("teste")
+    elif held_keys['d']: # rotate right
+        AGENT_RED.rotate_right()
+    elif held_keys['a']: # rotate left
+        AGENT_RED.rotate_left()
+    elif held_keys['s']: # rotate randomly
+        AGENT_RED.rotate_randomly()
+    elif held_keys['w']: # move forward
+        AGENT_RED.move()
 
 app.run()
