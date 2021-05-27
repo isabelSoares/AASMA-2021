418c731865f093d199a305460ea1940ec4e58d66
João Dias <joao.dias12321@gmail.com>
2021-05-27 15:30:26 +0100

    rlearning script

--- code/rlearning_test.py
+++ code/rlearning_test.py
@@ -1,7 +1,10 @@
+# script does not work without changing maze and RLearning agent
+# the script was being used to test different learning rates and discount factors
+
 import subprocess
 
-lr_list = [0.2, 0.4, 0.6, 0.9]
-gamma_list = [0.2, 0.4, 0.6, 0.9]
+lr_list = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+gamma_list = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
 for lr in lr_list:
     for gamma in gamma_list:
             result = subprocess.run('python .\maze.py rlearning map2 ' + str(lr) + ' ' + str(gamma), stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, shell=True)
